d8349eef17eff23c1d0481bb12d74191596753cd
BugHunter-X <askzenai@gmail.com>
2024-12-27 11:40:06 +0100

    Add bugSolution.cs file

---
+++ bugSolution.cs
@@ -0,0 +1,10 @@
+public class ExampleClass
+{
+    public int MyProperty { get; set; }
+
+    public void MyMethod()
+    {
+        MyProperty = 10; // Initialize MyProperty
+        Console.WriteLine(MyProperty); // Now this will print 10
+    }
+}
